84963466b3022ba1eb6d15065fed733c8598c341
Amir-Muradov <amirmuradov0631@gmail.com>
2023-12-19 09:41:37 +0400

    second js

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var str = prompt("Write the sentences")
+var texxt = str;(prompt("Write the number"))
+console.log(texxt);
+var choose = confirm("Write again")
+if (texxt = " "){
+    alert("The opperation is not correct")
+}else if(choose = true){
+    var texxt = str;(prompt("Write me number"))
+    console.log(texxt);
+}else{
+    console.log("You canceled")
+}
